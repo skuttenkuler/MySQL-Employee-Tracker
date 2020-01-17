21128b9ef065acd06f708da241c74af6c625bc71
skuttenkuler <samkiddmusic@gmail.com>
2020-01-17 15:36:42 -0800

    added foreign keys to Schema

--- db/schema.sql
+++ db/schema.sql
@@ -15,15 +15,16 @@ CREATE TABLE role(
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT,
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
+    FOREIGN KEY(name) REFERENCES department(name)
 );
 
 CREATE TABLE employee(
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT,
+    role_id VARCHAR(30),
     manager_id INT DEFAULT NULL,
     PRIMARY KEY (id)
+    FOREIGN KEY (title) REFERENCES role(title)
 );
