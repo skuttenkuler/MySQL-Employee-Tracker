0d21dfbb3eb6aaad3fffc803307937088c409c2e
skuttenkuler <sam.kuttenk@gmail.com>
2020-04-27 11:19:00 -0700

    redo server.js and update schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,30 +1,30 @@
-DROP DATABASE IF EXISTS employee_db;
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
 
-CREATE DATABASE employee_db;
+USE employees;
 
-USE employee_db;
-
-CREATE TABLE department(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30),
-    PRIMARY KEY (id)
-
+CREATE TABLE department (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
 );
 
-CREATE TABLE role(
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(name) REFERENCES department(name)
+CREATE TABLE role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
-CREATE TABLE employee(
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id VARCHAR(30),
-    manager_id INT DEFAULT NULL,
-    PRIMARY KEY (id)
-    FOREIGN KEY (title) REFERENCES role(title)
+CREATE TABLE employee (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,49 +1,33 @@
-                    -- Seed data for Employee Database --
--- Department --
-INSERT INTO department(name) VALUES("Finance");
-INSERT INTO department(name) VALUES("Engineering");
-INSERT INTO department(name) VALUES("Sales");
-INSERT INTO department(name) VALUES("Human Resources");
-INSERT INTO department(name) VALUES("Legal");
-INSERT INTO department(name) VALUES("Marketing");
+use employees;
 
+INSERT INTO department
+    (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
 
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
 
--- Roles --
-INSERT INTO role(title, salary) VALUES("Manager",250000.00 );
-INSERT INTO role(title, salary) VALUES("Account Manager",135000.00 );
-INSERT INTO role(title, salary) VALUES("Recruiter",135000.00 );
-INSERT INTO role(title, salary) VALUES("Training",140000.00 );
-INSERT INTO role(title, salary) VALUES("Product Manager",165000.00 );
-INSERT INTO role(title, salary) VALUES("Lead Engineer",180000.00 );
-INSERT INTO role(title, salary) VALUES("Software Engineer",135000.00 );
-INSERT INTO role(title, salary) VALUES("Junior Developer",90000.00 );
-INSERT INTO role(title, salary) VALUES("Mergers and Acquisitions",160000.00 );
-INSERT INTO role(title, salary) VALUES("Marketing Generalist",120000.00 );
-INSERT INTO role(title, salary) VALUES("Customer Support",80000.00 );
-INSERT INTO role(title, salary) VALUES("Intern",45000.00 );
-
-
-
-
-
-
-
-
-
-INSERT INTO employee(first_name ,last_name, role_id, manager_id) VALUES ("Patrick", "Bateman","Mergers and Acquisitions", 1390, NULL );
-INSERT INTO employee(first_name ,last_name, role_id, manager_id) VALUES ("Paul", "Allen","Account Manager", 1290, NULL );
-INSERT INTO employee(first_name ,last_name, role_id, manager_id) VALUES ("James", "Conway","Manager", 1290, NULL );
-INSERT INTO employee(first_name ,last_name, role_id, manager_id) VALUES ("Armand", "Goldman","Manager", 1290, NULL );
-INSERT INTO employee(first_name ,last_name, role_id, manager_id) VALUES ("Henry", "Hill","Manager", 1290, NULL );
-INSERT INTO employee(first_name ,last_name, role_id, manager_id) VALUES ("Tommy", "Devito","Account Manager", 1290, NULL );
-INSERT INTO employee(first_name ,last_name, role_id, manager_id) VALUES ("Tony", "Montana","Training", 1290, NULL );
-INSERT INTO employee(first_name ,last_name, role_id, manager_id) VALUES ("Elliot", "Alderson","Security Engineer", 1290, NULL );
-INSERT INTO employee(first_name ,last_name, role_id, manager_id) VALUES ("Kate", "Libby","Lead Engineer", 1290, NULL );
-INSERT INTO employee(first_name ,last_name, role_id, manager_id) VALUES ("Stanley", "Jobson","Software Engineer", 1290, NULL );
-INSERT INTO employee(first_name ,last_name, role_id, manager_id) VALUES ("Futaba", "Sakura","Software Engineer", 1290, NULL );
-INSERT INTO employee(first_name ,last_name, role_id, manager_id) VALUES ("Earl", "Stevens"," Sales Director", 1290, NULL );
-INSERT INTO employee(first_name ,last_name, role_id, manager_id) VALUES ("Carlton", "Banks"," Intern", 1290, NULL );
-INSERT INTO employee(first_name ,last_name, role_id, manager_id) VALUES ("Agador", "Sparticus"," Intern ", 1290, NULL );
-
-
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
